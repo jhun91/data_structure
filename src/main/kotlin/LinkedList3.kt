f547b992a6021b9cee0337de60d1b71ee7e10bc7
jhun91 <fonercis@naver.com>
2020-09-20 22:42:54 +0900

    더블 링크드리스트 실습 예정

--- src/main/kotlin/LinkedList3.kt
+++ src/main/kotlin/LinkedList3.kt
@@ -9,5 +9,24 @@ class Node03(
 )
 
 class NodeMgmt03(
+    private val data: Any,
+    private var head: Node03? = Node03(data),
+    private var tail: Node03? = head
+) {
 
-)
+    fun insert(data: Any) {
+        if (head == null) {
+            head = Node03(data)
+            tail = head
+        } else {
+            var node = head
+
+            while (node?.next != null) {
+                node = node.next //노드의 끝 찾기
+            }
+
+
+        }
+    }
+
+}
