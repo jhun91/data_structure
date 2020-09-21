315869280d59589309a4efcce7aeea96848e6f55
jhun91 <fonercis@naver.com>
2020-09-21 21:29:58 +0900

    더블링크드리스트 특정 노드의 앞, 뒤로 데이터 추가 실습

--- src/main/kotlin/LinkedList3.kt
+++ src/main/kotlin/LinkedList3.kt
@@ -1,5 +1,23 @@
 fun main() {
 
+    val doubleLinkedList = NodeMgmt03(0)
+
+    for (i in 1 until 10) {
+        doubleLinkedList.insert(i)
+    }
+
+    //doubleLinkedList.desc()
+
+    val node3 = doubleLinkedList.searchFromHead(3)
+    //println(node3?.data)
+
+    val node33 = doubleLinkedList.searchFromTail(3)
+    //println(node33?.data)
+
+    doubleLinkedList.insertBefore(1.5, 2)
+    doubleLinkedList.insertAfter(3.5, 3)
+
+    doubleLinkedList.desc()
 }
 
 class Node03(
@@ -14,7 +32,7 @@ class NodeMgmt03(
     private var tail: Node03? = head
 ) {
 
-    fun insert(data: Node03) {
+    fun insert(data: Any) {
         if (head == null) {
             head = Node03(data)
             tail = head
@@ -25,7 +43,7 @@ class NodeMgmt03(
                 node = node.next //노드의 끝 찾기
             }
 
-            var new = data //새로운 노드 데이터
+            val new = Node03(data) //새로운 노드 데이터
             node?.next = new
             new.prev = node
 
@@ -34,4 +52,90 @@ class NodeMgmt03(
         }
     }
 
+    fun desc() {
+        var node = head
+        while (node != null) {
+            println(node.data)
+            node = node.next
+        }
+    }
+
+    fun searchFromHead(data: Any): Node03? {
+        if (head == null) {
+            return null
+        }
+
+        var node = head
+        while (node != null) {
+            if (node.data == data) {
+                return node
+            } else {
+                node = node.next
+            }
+        }
+        return null
+    }
+
+    fun searchFromTail(data: Any): Node03? {
+        if (head == null) {
+            return null
+        }
+
+        var node = tail
+        while (node != null) {
+            if (node.data == data) {
+                return node
+            } else {
+                node = node.prev
+            }
+        }
+        return null
+    }
+
+    fun insertBefore(data: Any, beforeData: Any): Boolean {
+        if (head == null) {
+            head = Node03(data)
+            return true
+        } else {
+            var node = tail
+            while (node?.data != beforeData) {
+                node = node?.prev
+
+                if (node == null) {
+                    return false
+                }
+            }
+
+            val new = Node03(data)
+            val beforeNew = node.prev //원래 노드의 앞 노드
+            beforeNew?.next = new
+            new.prev = beforeNew
+            new.next = node
+            node.prev = new
+            return true
+        }
+    }
+
+    fun insertAfter(data: Any, afterData: Any): Boolean {
+        if (head == null) {
+            head = Node03(data)
+            return true
+        } else {
+            var node = head
+            while (node?.data != afterData) {
+                node = node?.next
+                if(node == null) {
+                    return false
+                }
+            }
+
+            val new = Node03(data)
+            val afterNew = node.next //원래 노드의 뒤 노드
+            afterNew?.prev = new
+            new.prev = node
+            new.next = afterNew
+            node.next = new
+            return true
+        }
+    }
 }
