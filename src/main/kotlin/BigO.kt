1944469d0f7150f684d6774f9ef3be5741a189ea
jhun91 <fonercis@naver.com>
2020-09-24 21:40:00 +0900

    1부터 n까지의 합 구하기 알고리즘 실습

--- src/main/kotlin/BigO.kt
+++ src/main/kotlin/BigO.kt
@@ -1,4 +1,19 @@
 fun main() {
 
-    //BigO 실습 예정
+    println(sumAll(100))
+}
+
+//1부터 n까지의 합 구하기
+fun sumAll(n: Int): Int {
+    /**
+     * 같은 방법이지만 for문은 O(n)     아래는 O(1)
+     */
+//    var total: Int = 0
+//
+//    for (num in 1 until n+1) {
+//        total += num
+//    }
+//    return total
+
+    return n * (n + 1) / 2
 }
